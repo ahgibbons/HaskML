43c68a543605555c372479f8b6180d88deb33e59
Andrew.Windows <andrew.h.gibbons@gmail.com>
2016-11-11 14:41:26 +0900

    Added PerceptronM, a matrix based Perceptron. Before starting Repa based implementation.

--- src/LinearClassifier.hs
+++ src/LinearClassifier.hs
@@ -1,13 +1,22 @@
 module LinearClassifier where
 
+import Data.Matrix
+
 type StandardizedSet = [[Double]]
-type Data = [Double]
-type TrainData = ([Data],[Bool])
+type Data = Matrix Double
+type TrainData = ([[Double]],[Bool])
 type UpdateParam = Double
 type Weight = Double
 
+toData :: [Double] -> Data
+toData ilist = fromList 1 (length ilist) ilist
+
+fromData :: Data -> [Double]
+fromData = toList
+
+
 class LinearClassifier p where
-  predict :: p -> Data -> Bool
+  predict :: p -> [Double] -> Bool
   fit :: Int -> [([Double],Bool)] -> p -> p
   weights :: p -> [Weight]
 
--- src/Perceptron.hs
+++ src/Perceptron.hs
@@ -1,18 +1,36 @@
 module Perceptron 
-( Perceptron(..))
+( Perceptron(..)
+, PerceptronM(..)
+, fitIter2
+, fitDatum2)
 where
 
 import Data.Either
 import Lib
 import LinearClassifier
+import Data.Matrix
 
 data Perceptron = Perceptron UpdateParam [Weight] deriving (Show)
+
+data PerceptronM = PerceptronM UpdateParam (Matrix Weight) deriving (Show)
 
 instance LinearClassifier Perceptron where
     predict (Perceptron _ ws) xs = weightScore ws xs > 0
     fit = fitIter
     weights (Perceptron _ ws) = ws
 
+
+listToMatrix :: [Double] -> Matrix Double
+listToMatrix list = fromList 1 (length list) list
+
+fitDatum2 :: ([Double],Bool) -> PerceptronM -> PerceptronM
+fitDatum2 (xs,yb) p@(PerceptronM eta ws) =
+    let i  = fromList 1 (length xs + 1) (1:xs)
+        y  = boolToNum yb
+        y' = boolToNum $ (getElem 1 1 $ i * ws) > 0
+        dw = scaleMatrix (eta*(y-y')) (transpose i)
+        ws' = ws + dw
+    in (PerceptronM eta ws')
     
 fitDatum :: ([Double],Bool) -> Perceptron -> Perceptron
 fitDatum (xs,yb) p@(Perceptron eta ws) =
@@ -23,10 +41,17 @@ fitDatum (xs,yb) p@(Perceptron eta ws) =
         ws'     = zipWith (+) ws updates
     in (Perceptron eta ws')
 
+
 fitData :: [([Double],Bool)] -> Perceptron -> Perceptron
 fitData tdata p = 
     foldr fitDatum p tdata  
 
+fitData2 :: [([Double],Bool)] -> PerceptronM -> PerceptronM
+fitData2 tdata p = 
+    foldr fitDatum2 p tdata
 
 fitIter :: Int -> [([Double],Bool)] -> Perceptron -> Perceptron
 fitIter n tdata p0 = iterate (fitData tdata) p0 !! n
+
+fitIter2 :: Int -> [([Double],Bool)] -> PerceptronM -> PerceptronM
+fitIter2 n tdata p0 = iterate (fitData2 tdata) p0 !! n
